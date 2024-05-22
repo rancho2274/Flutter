94ce0e49a98d9a624f72889721bb22b58c2271e3
Rachit Chandawar <130487757+rancho2274@users.noreply.github.com>
2024-05-22 23:58:00 +0530

    Add files via upload

--- assign_5.cpp
+++ assign_5.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 struct Node {
     int data;
     Node* left;
@@ -12,11 +11,9 @@ struct Node {
     Node(int val) : data(val), left(nullptr), right(nullptr) {}
 };
 
-
 Node* createNode(int val) {
     return new Node(val);
 }
-
 
 void insert(Node*& root, int val) {
     if (root == nullptr) {
@@ -35,23 +32,19 @@ void insert(Node*& root, int val) {
             if (current->left == nullptr) {
                 current->left = createNode(val);
                 return;
-            }
-            else {
+            } else {
                 s.push(current->left);
             }
-        }
-        else {
+        } else {
             if (current->right == nullptr) {
                 current->right = createNode(val);
                 return;
-            }
-            else {
+            } else {
                 s.push(current->right);
             }
         }
     }
 }
-
 
 void inorder(Node* root) {
     if (root == nullptr) return;
@@ -72,6 +65,52 @@ void inorder(Node* root) {
     }
 }
 
+void preorder(Node* root) {
+    if (root == nullptr) return;
+
+    stack<Node*> s;
+    s.push(root);
+
+    while (!s.empty()) {
+        Node* current = s.top();
+        s.pop();
+        cout << current->data << " ";
+
+        if (current->right) {
+            s.push(current->right);
+        }
+        if (current->left) {
+            s.push(current->left);
+        }
+    }
+}
+
+void postorder(Node* root) {
+    if (root == nullptr) return;
+
+    stack<Node*> s1, s2;
+    s1.push(root);
+
+    while (!s1.empty()) {
+        Node* current = s1.top();
+        s1.pop();
+        s2.push(current);
+
+        if (current->left) {
+            s1.push(current->left);
+        }
+        if (current->right) {
+            s1.push(current->right);
+        }
+    }
+
+    while (!s2.empty()) {
+        Node* current = s2.top();
+        s2.pop();
+        cout << current->data << " ";
+    }
+}
+
 int main() {
     Node* root = nullptr;
     int numNodes;
@@ -84,10 +123,17 @@ int main() {
         cin >> val;
         insert(root, val);
     }
-    
 
     cout << "Inorder traversal of the binary tree: ";
     inorder(root);
+    cout << endl;
+
+    cout << "Preorder traversal of the binary tree: ";
+    preorder(root);
+    cout << endl;
+
+    cout << "Postorder traversal of the binary tree: ";
+    postorder(root);
     cout << endl;
 
     return 0;
